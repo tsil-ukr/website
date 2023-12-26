929e16f01dc000ce9079a4261fd8404b224ee991
Davyd Kohut <davyd.kohut@gmail.com>
2023-12-27 00:21:41 +0200

    upd

---
+++ pages/docs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="UiDocsLayout">
+    <div class="UiDocsLayoutNavigation">
+      <div class="UiDocsLayoutNavigationHeader">
+        <NuxtLink to="/"><img src="@/assets/logo.png" alt=""></NuxtLink>
+      </div>
+    </div>
+    <main class="UiDocsLayoutMain">
+      //
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+.UiDocsLayout {
+  background: white;
+
+  display: grid;
+  grid-template-columns: 325px 1fr;
+  height: 100%;
+
+  .UiDocsLayoutNavigation {
+    border-right: 1px solid #FFE173;
+
+    .UiDocsLayoutNavigationHeader {
+      height: 6.5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 3.5rem;
+        height: 3.5rem;
+        object-fit: cover;
+      }
+    }
+  }
+}
+</style>
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="UiMain">
+    <div class="UiMainLogo">
+      <img src="@/assets/logo.png" alt="">
+      Ціль
+    </div>
+    <div class="UiMainTitle">
+      Швидка, проста, низькорівнева українська мова програмування.
+    </div>
+    <div class="UiMainCode">
+      <UiCodeBlock :code="`ціль україна = Атлантида{};`"/>
+    </div>
+    <div class="UiMainButtons">
+      <button class="UiMainButton">Завантажити</button>
+      <NuxtLink to="/docs" class="UiMainButton outline">Документація</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.UiMain {
+  height: 100%;
+  padding-top: 5rem;
+  display: flex;
+  flex-direction: column;
+
+  .UiMainLogo {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+
+    img {
+      margin-bottom: 1rem;
+      width: 5rem;
+      height: 5rem;
+      object-fit: contain;
+    }
+  }
+
+  .UiMainTitle {
+    margin: 0 auto;
+    margin-top: 1rem;
+    max-width: 30ch;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: 500;
+    text-align: center;
+  }
+
+  .UiMainCode {
+    margin: 0 auto;
+    margin-top: 2rem;
+    max-width: 500px;
+    width: 100%;
+  }
+
+  .UiMainButtons {
+    margin: 0 auto;
+    margin-top: 2rem;
+    max-width: calc(500px + 1rem);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+
+    .UiMainButton {
+      height: 3rem;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #9B503A;
+      color: white;
+      border: 4px solid #9B503A;
+      font-size: 1rem;
+      font-weight: 500;
+      text-decoration: none;
+
+      &:hover {
+        background: #1D1315;
+        color: white;
+        border: 4px solid #1D1315;
+        cursor: pointer;
+      }
+
+      &:active {
+        background: black;
+        color: white;
+        border: 4px solid black;
+      }
+
+      &.outline {
+        background: transparent;
+        border: 4px solid #9B503A;
+        color: #9B503A;
+
+        &:hover {
+          background: #1D1315;
+          color: white;
+          border: 4px solid #1D1315;
+          cursor: pointer;
+        }
+
+        &:active {
+          background: black;
+          color: white;
+          border: 4px solid black;
+        }
+      }
+    }
+  }
+}
+
+.UiLeopard {
+  width: 100%;
+
+  img {
+    width: 100%;
+  }
+}
+</style>

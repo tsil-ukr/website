4b9ea5bd5f5e80e934966d067c4b049c1d0505c1
Davyd Kohut <davyd.kohut@gmail.com>
2023-12-27 20:50:10 +0200

    upd

--- pages/docs.vue
+++ pages/docs.vue
@@ -4,17 +4,19 @@
       <div class="UiDocsLayoutNavigationHeader">
         <NuxtLink to="/"><img src="@/assets/logo.png" alt=""></NuxtLink>
       </div>
+      <div class="UiDocsLayoutNavigationLinks">
+        <a href="/" class="UiDocsLayoutNavigationLink">Вступ</a>
+        <a href="/" class="UiDocsLayoutNavigationLink active">Встановлення</a>
+        <a href="/" class="UiDocsLayoutNavigationLink">Перша програма</a>
+      </div>
     </div>
     <main class="UiDocsLayoutMain">
-      //
     </main>
   </div>
 </template>
 
 <style lang="scss">
 .UiDocsLayout {
-  background: white;
-
   display: grid;
   grid-template-columns: 325px 1fr;
   height: 100%;
@@ -32,6 +34,32 @@
         width: 3.5rem;
         height: 3.5rem;
         object-fit: cover;
+      }
+    }
+
+    .UiDocsLayoutNavigationLinks {
+      display: flex;
+      flex-direction: column;
+
+      .UiDocsLayoutNavigationLink {
+        display: flex;
+        align-items: center;
+        text-align: left;
+        padding: 0.75rem 1.5rem;
+        color: #1D1315;
+        text-decoration: none;
+        font-size: 1rem;
+        border-bottom: 1px solid #FFE173;
+
+        &:first-child {
+          border-top: 1px solid #FFE173;
+        }
+
+        &.active {
+          background: #FFE173;
+          color: #1D1315;
+          font-weight: 500;
+        }
       }
     }
   }

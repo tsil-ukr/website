a2c08bc481f24f8c80794c09c5bfb2fe8bc3ed58
Davyd Kohut <davyd.kohut@gmail.com>
2023-12-30 00:54:40 +0200

    upd

--- pages/документація/вказівки.vue
+++ pages/документація/вказівки.vue
@@ -13,5 +13,29 @@ definePageMeta({
 <template>
   <UiDocsWrapper>
     <h1 class="UiDocsLayoutTitle">Вказівки</h1>
+    <p>Додати</p>
+    <UiCodeBlock :code="`а + б`" borderless />
+    <p>Відняти</p>
+    <UiCodeBlock :code="`а - б`" borderless />
+    <p>Помножити</p>
+    <UiCodeBlock :code="`а * б`" borderless />
+    <p>Поділити</p>
+    <UiCodeBlock :code="`а / б`" borderless />
+    <p>Остача</p>
+    <UiCodeBlock :code="`а % б`" borderless />
+    <p>Степінь</p>
+    <UiCodeBlock :code="`а ** б`" borderless />
+    <p>Чи рівно</p>
+    <UiCodeBlock :code="`а == б`" borderless />
+    <p>Чи не рівно</p>
+    <UiCodeBlock :code="`а != б`" borderless />
+    <p>Чи більше</p>
+    <UiCodeBlock :code="`а > б`" borderless />
+    <p>Чи менше</p>
+    <UiCodeBlock :code="`а < б`" borderless />
+    <p>Чи більше або рівно</p>
+    <UiCodeBlock :code="`а >= б`" borderless />
+    <p>Чи менше або рівно</p>
+    <UiCodeBlock :code="`а <= б`" borderless />
   </UiDocsWrapper>
 </template>

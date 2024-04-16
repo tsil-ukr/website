5c38fe5a0267c2a64a7fb7febf1982b01fb0bbf8
Davyd Kohut <davyd.kohut@gmail.com>
2024-04-16 21:25:12 +0300

    🧬

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,40 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
 
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [
+        {
+          name: "description",
+          content: "Компільована українська мова програмування.",
+        },
+        {
+          name: "keywords",
+          content:
+            "ціль, мова програмування, програмування, українська мова програмування, українська мова, ціль мова програмування, ціль мова",
+        },
+      ],
+      title: "Ціль",
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap",
+        },
+      ],
+    },
+  },
+
   nitro: {
     prerender: {
       routes: [

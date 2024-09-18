ad6ad159afcec69d96360e0671285d5a1272865a
Davyd Kohut <davyd.kohut@gmail.com>
2024-09-18 18:10:14 +0300

    🧬

--- згенерувати.sh
+++ згенерувати.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env sh
 
-VERSION="$1"
-
-if [ -z "$VERSION" ]; then
-    echo "Версія не вказана"
-    exit 1
-fi
-
-if [ -d ".будування/$VERSION" ]; then
-  read -p "Версія $VERSION вже існує. Перезаписати? [так/ні] " -r
-  if [ "$REPLY" != "так" ]; then
-    echo "Операцію скасовано"
-    exit 0
-  fi
-  echo "Будуємо $VERSION"
-fi
+VERSION="$(cat "ВЕРСІЯ")"
 
 rm -rf ".будування/$VERSION"
 
@@ -22,5 +8,3 @@ rm -rf ".будування/$VERSION"
 cp -a ресурси index.html 404.html .будування
 докс карта --вхід=.будування --вихід=.будування --домен=ціль.укр
 find .будування -type f -name "*.html" -exec sed -i "s/{{ВЕРСІЯ_ЦІЛІ}}/$VERSION/g" {} \;
-
-echo "Готово"
